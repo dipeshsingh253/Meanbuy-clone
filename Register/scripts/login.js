7651d0c3fa5cd4e3e1f9a7fef145911d7241de87
Abhra11 <abhra001597@gmail.com>
2022-07-21 22:33:49 +0530

    day3 done

---
+++ Register/scripts/login.js
@@ -0,0 +1,32 @@
+let dataArrLS = JSON.parse(localStorage.getItem("userData")) || [];
+
+
+
+function login(){
+   
+
+    let signinData = {
+                       email:document.querySelector("#email").value,
+                       password:document.querySelector("#pass1").value,
+                     };
+                     if(checkSignin(signinData.email,signinData.password)===true){
+                        localStorage.setItem("signin",signinData)
+                        alert("Log in Successfull")
+                        window.location.href="index.html";
+                     }else{
+                        alert("Wrong Email or Password");
+                        window.location.reload();
+                     }
+}
+
+function checkSignin(email,password){
+    let filtered = dataArrLS.filter(function(element){
+ return element.email===email && element.password===password; 
+    });
+
+    if(filtered.length>0){
+        return true;
+    }else{
+        return false;
+    }
+}
